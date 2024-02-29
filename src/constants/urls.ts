72a2cbeb36430835abb1a84474166dfc90f34fa1
Gydini13 <gydini13@gmail.com>
2024-02-29 19:30:38 +0200

    take one film by id

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -2,6 +2,7 @@ const baseURL = 'https://api.themoviedb.org/3';
 
 const urls = {
     movies: '/discover/movie',
+    movieById: '/movie',
     genres: '/genre/movie/list',
     search: '/search/movie?&query=',
     poster: 'https://image.tmdb.org/t/p/w500'
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -6,7 +6,7 @@ import {IRes} from "../types";
 const movieService = {
     getAll: (page: number = 1):IRes<IMovie[]> => apiService.get(urls.movies, {params: {page}}),
     search: (word: string) => apiService.get(`${urls.search}${word}`),
-    getById: (id: number): IRes<IResult> => apiService.get(`${urls.movies}/${id}`)
+    getById: (id: number): IRes<IResult> => apiService.get(`${urls.movieById}/${id}`)
 }
 
 
